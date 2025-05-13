242d3bb1ff90170d9e12e391eb8f57e6e67d5dc6
Keerthana13032003 <155966016+Keerthana13032003@users.noreply.github.com>
2025-05-13 10:23:24 +0530

    changed

--- app.py
+++ app.py
@@ -194,5 +194,9 @@ async def predict_pcos(
     })
 
 
+#if __name__ == "__main__":
+    #uvicorn.run(app, host="127.0.0.1", port=5000)
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use $PORT from environment or default to 5000 locally
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
